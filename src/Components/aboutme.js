b62786fccd10d6faddb4949395a5b86272e9cb70
Nandakumaran <janakan@MacBook-Pro-de-Janakan.local>
2018-12-31 16:58:05 +0100

    Setup the page access

---
+++ src/Components/aboutme.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Aboutme extends Component {
+    render() {
+        return (
+            <div><h1>About me </h1></div>
+        )
+    }
+}
+
+export default Aboutme;
