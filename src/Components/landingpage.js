90f67feb5134692a372e42a1de94e6d0c2eef200
Nandakumaran <janakan@MacBook-Pro-de-Janakan.local>
2019-01-17 23:25:36 +0100

    do the project page

--- src/Components/landingpage.js
+++ src/Components/landingpage.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import {Grid, Cell} from "react-mdl";
 
 class LandingPage extends Component {
     render() {
         return (
-            <div><h1>Landing Page </h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src="https://media.licdn.com/dms/image/C4D03AQE5Wxpkz_Y8Pw/profile-displayphoto-shrink_200_200/0?e=1551916800&v=beta&t=pHkS5VbGxWf6VEfaxN_PvOZmMlXgzPkja1fB_iGtA7g"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Full-stack Developer</h1>
+                        <hr/>
+
+                            <p>HTML/CSS | Bootstrap | Javascript | React | React Native | NodeJS | Express | MongoDB</p>
+
+                            <div className="social-links">
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/janakan-nandakumaran/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                </a>
+                                {/* Github */}
+                                <a href="https://github.com/JanakanNan" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true"/>
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,9 +1,93 @@
 import React, { Component } from 'react';
+import {Tab, Tabs, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText} from "react-mdl";
 
 class Projects extends Component {
+    constructor(props){
+        super(props);
+        this.state = {activeTab: 0};
+    }
+
+    toggleCategories(){
+        if(this.state.activeTab === 0){
+            return(
+                <div className="projects-grid">
+                {/* Project 1 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background:'url(https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414?w=256&h=256&expires=1547769600000&ixlib=js-1.2.0&s=1b416396b4dfc11c36d7f00f931c90ae) center / cover'}}>React Project #1</CardTitle>
+                    <CardText>
+                        Lorem Ipsum is simply dummy text of the priting and typesetting industry. Lorem Ipsum has the industry's
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
+                {/* Project 2 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background:'url(https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414?w=256&h=256&expires=1547769600000&ixlib=js-1.2.0&s=1b416396b4dfc11c36d7f00f931c90ae) center / cover'}}>React Project #2</CardTitle>
+                    <CardText>
+                        Lorem Ipsum is simply dummy text of the priting and typesetting industry. Lorem Ipsum has the industry's
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
+                {/* Project 3 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background:'url(https://spectrum.imgix.net/communities/102b08e1-f26e-4cda-b252-03258776bc14/react.png.0.1901592707012414?w=256&h=256&expires=1547769600000&ixlib=js-1.2.0&s=1b416396b4dfc11c36d7f00f931c90ae) center / cover'}}>React Project #3</CardTitle>
+                    <CardText>
+                        Lorem Ipsum is simply dummy text of the priting and typesetting industry. Lorem Ipsum has the industry's
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>CodePen</Button>
+                        <Button colored>LiveDemo</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
+                </div>
+             )
+        }else if (this.state.activeTab === 1){
+            return(
+                <div><h1>This is Angular</h1></div>
+            )
+        }else if (this.state.activeTab === 2){
+            return(
+                <div><h1>This is VueJS</h1></div>
+            )
+        }else if (this.state.activeTab === 3){
+            return(
+                <div><h1>This is MongoDB</h1></div>
+            )
+        }
+    }
     render() {
         return (
-            <div><h1>Projects Page </h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Angular</Tab>
+                    <Tab>VueJS</Tab>
+                    <Tab>MongoDB</Tab>
+                </Tabs>
+
+                <Grid>
+                    <Cell col={12}>
+                        <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
